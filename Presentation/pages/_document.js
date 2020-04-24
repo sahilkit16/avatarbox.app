3f2847d5cac4b274b7ca8f478d10ea470713702a
Stephen Tillman <askmrtillman@gmail.com>
2020-04-24 17:04:46 -0400

    add app manifest, document head + page title

---
+++ Presentation/pages/_document.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-danger */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { useAmp } from 'next/amp';
+
+function AmpWrap({ ampOnly, nonAmp }) {
+  const isAmp = useAmp();
+  if (ampOnly) return isAmp && ampOnly;
+  return !isAmp && nonAmp;
+}
+
+class AvatarBoxSite extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <meta charSet="utf-8" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="../images/fav_icon.png" type="image/x-icon"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet"/>
+          <link rel="stylesheet" href="https://unpkg.com/bulma@0.8.0/css/bulma.min.css" />
+          <link rel="stylesheet" type="text/css" href="../css/landing.css"/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AvatarBoxSite;
--- Presentation/pages/index.js
+++ Presentation/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
 
 class Index extends React.Component {
     render(){
-        return <p>home page</p>
+        return (
+          <div>
+              <Head>
+                  <title>Sign Up | AvatarBox</title>
+              </Head>
+              <p>home page</p>
+          </div>
+        );
     }
 }
 
