3dcccd09752e08e4096980f39de1678801930121
Stephen Tillman <askmrtillman@gmail.com>
2020-04-27 22:46:54 -0400

    stub cache service

---
+++ Services/cache.service.js
@@ -0,0 +1,14 @@
+const _cache = {};
+
+// TODO: implement using redis client
+
+class CacheService {
+    static get(value) {
+        return _cache[value];
+    }
+    static set(key, value) {
+        _cache[key] = value;
+    }
+}
+
+module.exports=CacheService;
