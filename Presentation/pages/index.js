5e4e685bfb9ca6aef1359613b418609a34a293ac
Stephen Tillman <askmrtillman@gmail.com>
2020-04-27 22:51:00 -0400

    pass user query param from url into HeroBody component

--- Presentation/pages/index.js
+++ Presentation/pages/index.js
@@ -4,11 +4,18 @@ import HeroBody from '../components/hero-body';
 import HeroSection from '../components/hero-section';
 
 class Index extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  static getInitialProps = async ctx => {
+    return { user: ctx.query.user };
+  }
   render() {
     return (
       <HeroSection title="Sign Up | Avatar Box">
         <HeroHead />
         <HeroBody
+          user={this.props.user}
           headline="New Avatar, Daily"
           proposition="A handy resource to update your Gravatar icon"
         />
