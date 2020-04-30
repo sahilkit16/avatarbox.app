8758b6567ef7d57b4348cb6e28d9b82f20f155c3
Stephen Tillman <askmrtillman@gmail.com>
2020-04-29 21:07:40 -0400

    incorporate rsa encryption service

--- Presentation/routes/encrypt.js
+++ Presentation/routes/encrypt.js
@@ -1,15 +1,11 @@
 const { Router } = require('express');
-const { exec } = require('child_process');
 const router = Router();
+const RsaService = require('../../Services/rsa.service');
 
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
     const { password } = req.body;
-    exec(`echo ${password} | openssl rsautl -encrypt -inkey rsa.public -pubin | base64 -w 0`,
-    (err, stdout) => {
-        if(err) throw err;
-        const ciphertext = stdout.trim();
-        res.render("ciphertext", { ciphertext });
-    });
+    const ciphertext = await RsaService.encrypt(password);
+    res.render("ciphertext", { ciphertext });
 });
 
 module.exports = router;
