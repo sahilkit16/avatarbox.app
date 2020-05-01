972a742d4a55b29a69ca9a9c8f95c81904bddf54
Stephen Tillman <askmrtillman@gmail.com>
2020-05-01 12:57:45 -0400

    rename ciphertext.pug to encrypt.pug; fix breaking changes

--- Presentation/routes/encrypt.js
+++ Presentation/routes/encrypt.js
@@ -5,7 +5,7 @@ const RsaService = require('../../Services/rsa.service');
 router.post('/', async (req,res) => {
     const { password } = req.body;
     const ciphertext = await RsaService.encrypt(password);
-    res.render("ciphertext", {
+    res.render("encrypt", {
         title: "Encrypt | Avatar Box",
         ciphertext
     });
