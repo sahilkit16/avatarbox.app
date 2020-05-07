b7851d103288a7ac46ec072907d78812ddd4d9d9
Stephen Tillman <askmrtillman@gmail.com>
2020-05-07 13:14:30 -0400

    update error codes; add link to wiki page

--- Domain/error-code.js
+++ Domain/error-code.js
@@ -1,4 +1,10 @@
+// https://bitbucket.org/mrtillman/avatarbox.web/wiki/Error%20Codes
+
 module.exports = {
-  NoImages: 0,
-  SingleImage: 1,
+  MissingEmail: 1000,
+  MissingPassword: 1001,
+  InvalidEmail: 1002,
+  InvalidPassword: 1003,
+  NoImages: 2000,
+  SingleImage: 2001,
 };
