4e2d1914aceb5c425635cacdd53630f4650b7bff
Stephen Tillman <askmrtillman@gmail.com>
2020-05-14 01:18:52 -0400

    configure jest + enzyme; add test scripts + install testing utilities

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  
+  collectCoverageFrom: [
+    'Application/**/*.{js,jsx}',
+  ],
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+
+  //https://jestjs.io/docs/en/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+  transform: {
+    "^.+\\.(jsx|js)?$": "<rootDir>/node_modules/babel-jest"
+  },
+
+  // https://mongoosejs.com/docs/jest.html
+  testEnvironment: 'node',
+};
