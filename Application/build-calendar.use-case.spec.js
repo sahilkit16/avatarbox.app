3e63137beed68f3ded54d90f9528c4585059a08d
Stephen Tillman <askmrtillman@gmail.com>
2020-05-17 17:33:56 -0400

    stub user service

--- Application/build-calendar.use-case.spec.js
+++ Application/build-calendar.use-case.spec.js
@@ -3,7 +3,9 @@ const BuildCalendarUseCase = require('./build-calendar.use-case');
 describe('BuildCalendarUseCase', () => {
   let useCase;
   beforeEach(() => {
-    useCase = new BuildCalendarUseCase();
+    useCase = new BuildCalendarUseCase({
+      userService: {}
+    });
   })
   it('should work', () => {
     expect(useCase.execute).toBeDefined();
