c2e5c425dde1b96a2379b3aa264c6008231d47c2
Stephen Tillman <askmrtillman@gmail.com>
2020-05-17 23:55:02 -0400

    create data store module

---
+++ Infrastructure/data-store.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Logger = require("../Common/logger");
+
+class DataStore {
+  constructor(logger = new Logger()){
+    this.logger = logger;
+  }
+  async connect() {
+    // https://mongoosejs.com/docs/deprecations.html#findandmodify
+    mongoose.set("useFindAndModify", false);
+    return new Promise((resolve, reject) => {
+      mongoose.connect(
+        `mongodb://avatarbox:27017/avbx`,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (error) => {
+          if (error) reject(new Error("db connection failed"));
+          this.logger.notice("db connected");
+          resolve(true);
+        }
+      );
+    })
+  }
+  async disconnect(done) {
+    mongoose.disconnect();
+    if(done) done();
+    this.logger.warn("db disconnected");
+  }
+}
+
+module.exports = DataStore;
