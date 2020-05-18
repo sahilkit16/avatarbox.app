682eb57a89d4f0b4646aa6e36cdda4ba2d424758
Stephen Tillman <askmrtillman@gmail.com>
2020-05-17 23:55:42 -0400

    stub e2e tests

--- Tests/app.e2e-spec.js
+++ Tests/app.e2e-spec.js
@@ -1,5 +1,22 @@
+const request = require('supertest');
+const { app } = require('../Presentation/app');
+const DataStore = require('../Infrastructure/data-store');
+
+const dataStore = new DataStore();
+
 describe('app', () => {
+  beforeAll(async () => {
+    await dataStore.connect();
+  })
   it('should work', () => {
-    expect(true).toBe(true);
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+      });
+  })
+  afterAll(done => {
+    dataStore.disconnect(done);
   })
 })
