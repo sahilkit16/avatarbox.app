ce7e563533f9158927147b1a65b5b0358b68bf15
Stephen Tillman <askmrtillman@gmail.com>
2020-05-19 16:32:56 -0400

    add validation logic for feedback form; style form validation messages

--- Presentation/routes/feedback.js
+++ Presentation/routes/feedback.js
@@ -1,35 +1,64 @@
 require('dotenv').config();
 const { Router } = require("express");
 const fetch = require('node-fetch');
+const EmailValidator = require("email-validator");
 const ThanksView = require('../view-models/thanks');
+const FeedbackView = require('../view-models/feedback');
 const router = Router();
 const Logger = require("../../Common/logger");
 
 const logger = new Logger();
-const model = new ThanksView();
+const thanksModel = new ThanksView();
 
 router.post("/", async (req, res) => {
-  const feedback = req.body;
-  // TODO: form validation
-  if(!feedback.comments) {
-    logger.warn("aborting feedback - no comments provided");
-    res.render('thanks', model);
+  
+  // TODO: isolate validation logic
+
+  const feedbackModel = new FeedbackView();
+  feedbackModel.event_id = req.session.eventId;
+  feedbackModel.name = req.body.name;
+  feedbackModel.email = req.body.email;
+  feedbackModel.comments = req.body.comments;
+
+  const validationSummary = {};
+  const requiredFieldMessage = "This field is required";
+  validationSummary.name = feedbackModel.name ? null : requiredFieldMessage;
+  validationSummary.email = feedbackModel.email ? null : requiredFieldMessage;
+  validationSummary.comments = feedbackModel.comments ? null : requiredFieldMessage;
+  
+  if(feedbackModel.email && !EmailValidator.validate(feedbackModel.email)){
+    validationSummary.email = "Invalid Email";
   }
+
+  if(validationSummary.name 
+    || validationSummary.email 
+    || validationSummary.comments) {
+    feedbackModel.validationSummary = validationSummary;
+    return res.render('feedback', feedbackModel);
+  }
+  
+  if(!!process.env.DEV_ENV){
+    req.session.eventId = null;
+    return res.render('thanks', thanksModel);
+  }
+  
   fetch("https://sentry.io/api/0/projects/avatar-box/avatarboxweb/user-feedback/",{
     method: "POST",
     headers: {
       "authorization": `DSN ${process.env.SENTRY_SOURCE}`,
       "content-type": "application/json",
     },
-    body: JSON.stringify(feedback)
+    body: JSON.stringify(feedbackModel)
   }).then(async (_res) => {
     if (_res.ok) {
-      res.render('thanks', model);
+      res.render('thanks', thanksModel);
+      req.session.eventId = null;
     } else {
+      req.session.eventId = null;
       const message = await _res.text();
       logger.warn("no feedback submitted");
       logger.warn(`${message || _res.statusText}`);
-      res.render('thanks', model);
+      res.render('thanks', thanksModel);
     }
   });
 })
---
+++ Presentation/view-models/feedback.js
@@ -0,0 +1,18 @@
+const ThanksView = require("./thanks");
+
+class FeedbackView extends ThanksView {
+  constructor() {
+    super();
+    this.event_id = 0;
+    this.name = null;
+    this.email = null;
+    this.comments = null;
+    this.validationSummary = {
+      name: null,
+      email: null,
+      comments: null
+    };
+  }
+}
+
+module.exports = FeedbackView;
