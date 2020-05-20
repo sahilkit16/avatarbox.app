1731128bf4223a7eb229324aeea3e9b801cbda73
Stephen Tillman <askmrtillman@gmail.com>
2020-05-20 15:32:50 -0400

    style feedback form

--- Presentation/components/oops.js
+++ Presentation/components/oops.js
@@ -20,12 +20,43 @@ export default function Oops({ title, message, eventId }) {
           </div>
           <div className="level script-enabled cloak">
             <div className="level-item">
-              <button id="btn-feedback" class="button is-info">
+              <button id="btn-feedback" className="button is-info">
                 Report Feedback
               </button>
             </div>
           </div>
         </section>
+        <noscript>
+          <div className="level">
+              <div className="level-item">
+                <div className="box" id="feedback-form">
+                    <div>
+                      <p className="is-size-4 has-text-gray has-text-centered">It looks like we're having some internal issues.</p>
+                      <p className="is-size-6 has-text-grey-light has-text-centered">Our team has been notified. If you'd like to help, tell us what happened below.</p>
+                    </div>
+                    <form className="form" action="/feedback" method="POST">
+                      <div className="field">
+                          <label className="label">
+                            Name
+                            <div className="control"><input className="input is-medium" name="name" type="text" placeholder="Mary Mack"/></div>
+                          </label>
+                      </div>
+                      <div className="field">
+                          <label className="label">
+                            Email
+                            <div className="control"><input className="input is-medium" name="email" type="email" placeholder="mary@example.com"/></div>
+                          </label>
+                      </div>
+                      <div className="field"><label className="label">What Happened?<textarea className="textarea is-medium" name="comments" placeholder="I clicked on 'X' and then hit 'Confirm'"></textarea></label></div>
+                      <button className="button is-info is-pulled-right" type="submit">Submit</button>
+                    </form>
+                </div>
+              </div>
+          </div>
+          <div className="level">
+              <div className="level-item"><a className="button is-info" href="#here" id="btn-feedback">Report Feedback</a></div>
+          </div>
+        </noscript>
       </div>
     </div>
   );
