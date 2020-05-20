17f34dc6a75f5694a75eb48f738233707f6ee222
Stephen Tillman <askmrtillman@gmail.com>
2020-05-20 15:35:17 -0400

    add dummy content to test custom error pages

---
+++ Presentation/pages/broken.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Error from "../pages/_error";
+
+function BrokenComponent({ breakit }){
+  if(breakit){
+    throw new Error("broken child component");
+  }
+  return <div />;
+}
+
+class BrokenPage extends React.Component {
+  constructor(){
+    super();
+    this.state = { breakit: false }
+    this.jsError = this.jsError.bind(this);
+    this.reactError = this.reactError.bind(this);
+  }
+  
+  jsError(){
+    try {
+      _notDefined();
+    } catch (error) {
+      // TODO: log error to Sentry
+     this.setState({ hasError: true });
+    }
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(err, errInfo){
+    console.log(err, errInfo);
+  }
+
+  reactError(){
+    this.setState({ breakit: true });
+  }
+  
+  render(){
+    if(this.state.hasError){
+      return <Error />
+    }
+    return (
+      <div>
+        <p>
+          <a href="/server-error">Trigger 500 Server Error</a>
+        </p>
+        <p>
+          <button onClick={this.jsError}>Trigger Javascript Error</button>
+        </p>
+        <p>
+          <button onClick={this.reactError}>Trigger React Error</button>
+        </p>
+        <BrokenComponent breakit={this.state.breakit} />
+      </div>
+    )
+  }
+}
+
+export default BrokenPage;
---
+++ Presentation/pages/server-error.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ServerErrorPage extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return null;
+  }
+}
+
+ServerErrorPage.getInitialProps = async () => {
+  notDefined();
+}
+
+export default ServerErrorPage;
