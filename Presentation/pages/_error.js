6f17b374c6a33e707a2b0fc0a54bb6242f93ca82
Stephen Tillman <askmrtillman@gmail.com>
2020-05-21 10:01:51 -0400

    submit crash report; capture event id

--- Presentation/pages/_error.js
+++ Presentation/pages/_error.js
@@ -3,10 +3,12 @@ import HeroSection from "../components/hero-section";
 import ErrorBody from "../components/error-body";
 import ErrorView from "../view-models/error";
 import { useEffect } from "react";
+import * as Sentry from "@sentry/browser";
 
-function Error({ statusCode }) {
+function Error({ statusCode, eventId }) {
   const model = new ErrorView();
   model.title = "Error | Avatar Box";
+  model.eventId = eventId;
   if (statusCode) {
     model.title = `${statusCode} ${model.title}`;
     model.message = `A ${statusCode} server error has occurred.`;
@@ -16,19 +18,23 @@ function Error({ statusCode }) {
 
   useEffect(() => {
     site.uncloak();
+    if(typeof window != "undefined"){
+      window.eventId = eventId;
+    }
   });
 
   return (
     <HeroSection hideCoverImage="true">
       <HeroHead title={model.title} navbar={model.navbar} />
-      <ErrorBody message={model.message} />
+      <ErrorBody message={model.message} eventId={eventId}/>
     </HeroSection>
   );
 }
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+  const eventId = Sentry.captureException(err);
+  return { statusCode, eventId };
 };
 
 export default Error;
