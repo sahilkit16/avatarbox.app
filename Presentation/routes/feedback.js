d4940a7797687285f98e431ab0cb88501ba38485
Stephen Tillman <askmrtillman@gmail.com>
2020-05-22 13:54:08 -0400

    use feedback view model

--- Presentation/routes/feedback.js
+++ Presentation/routes/feedback.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { Router } = require("express");
 const fetch = require("node-fetch");
-const EmailValidator = require("email-validator");
 const ThanksVM = require("../view-models/thanks.vm");
 const FeedbackVM = require("../view-models/feedback.vm");
 const router = Router();
@@ -11,44 +10,25 @@ const logger = new Logger();
 const thanksModel = new ThanksVM();
 
 router.post("/", async (req, res) => {
-  // TODO: isolate validation logic
 
-  const feedbackModel = new FeedbackVM();
-  feedbackModel.eventId = (req.body.eventId || req.session.eventId);
-  feedbackModel.name = req.body.name;
-  feedbackModel.email = req.body.email;
-  feedbackModel.comments = req.body.comments;
-
-  const errors = {};
-  const requiredFieldMessage = "This field is required";
-  errors.name = feedbackModel.name ? null : requiredFieldMessage;
-  errors.email = feedbackModel.email ? null : requiredFieldMessage;
-  errors.comments = feedbackModel.comments
-    ? null
-    : requiredFieldMessage;
-
-  if (feedbackModel.email && !EmailValidator.validate(feedbackModel.email)) {
-    errors.email = "Invalid Email";
-  }
+  const feedbackVm = new FeedbackVM();
+  feedbackVm.eventId = (req.body.eventId || req.session.eventId);
+  feedbackVm.name = req.body.name;
+  feedbackVm.email = req.body.email;
+  feedbackVm.comments = req.body.comments;
 
   if (
-    errors.name ||
-    errors.email ||
-    errors.comments
+    feedbackVm.errors.name ||
+    feedbackVm.errors.email ||
+    feedbackVm.errors.comments
   ) {
-    feedbackModel.errors = errors;
-    return res.render("feedback", feedbackModel);
+    return res.render("feedback", feedbackVm);
   }
 
-  if (!!process.env.DEV_ENV) {
+  if (process.env.DEV_ENV) {
     req.session.eventId = null;
     return res.render("thanks", thanksModel);
   }
-  
-  // rename event id field for Sentry
-  // see https://docs.sentry.io/api/projects/post-project-user-reports/
-  feedbackModel.event_id = feedbackModel.eventId;
-  delete feedbackModel.eventId;
 
   fetch(
     "https://sentry.io/api/0/projects/avatar-box/avatarboxweb/user-feedback/",
@@ -58,7 +38,7 @@ router.post("/", async (req, res) => {
         authorization: `DSN ${process.env.SENTRY_DSN}`,
         "content-type": "application/json",
       },
-      body: JSON.stringify(feedbackModel),
+      body: JSON.stringify(feedbackVm.asPOJO()),
     }
   ).then(async (_res) => {
     if (_res.ok) {
--- Presentation/view-models/feedback.vm.js
+++ Presentation/view-models/feedback.vm.js
@@ -1,7 +1,61 @@
+const validator = require('validator').default;
 const ThanksVM = require("./thanks.vm");
 
+const getProxy = (vm) => {
+  return new Proxy(vm, {
+    set: (target, key, value) => {
+      
+      if(key == "eventId"){
+        if(value && typeof value == "string"){
+          target[key] = value;
+          return true;
+        }
+        return false;
+      }
+
+      if(key != "name" && key != "email" && key != "comments"){
+        return true;
+      }
+      
+      target[key] = value;
+
+      if(!value){
+        target.errors[key] = "This field is required";
+        return true;
+      }
+  
+      if(key == "email"){
+        
+        if(validator.isEmail(value)){
+          target.errors[key] = null;
+        } else {
+          target.errors[key] = "Invalid email";
+          return true;
+        }
+      }
+      
+      if((key == "name" || key =="email") && value.length > 100){
+        target.errors[key] = `Must be 100 characters or less`;
+        return true;
+      } else {
+        target.errors[key] = null;
+      }
+      
+      if(key == "comments" && value.length > 500){
+        target.errors[key] = `Must be 500 characters or less`;
+        return true;
+      } else {
+        target.errors[key] = null;
+      }
+
+      return true;
+
+    }
+  });
+}
+
 class FeedbackVM extends ThanksVM {
-  constructor() {
+  constructor(){
     super();
     const requiredFieldMessage = "This field is required";
     this.eventId = null;
@@ -13,6 +67,19 @@ class FeedbackVM extends ThanksVM {
       email: requiredFieldMessage,
       comments: requiredFieldMessage,
     };
+    return getProxy(this);
+  }
+
+  asPOJO() {
+    return {
+      name: this.name,
+      email: this.email,
+      comments: this.comments,
+
+      // rename event id field for Sentry
+      // see https://docs.sentry.io/api/projects/post-project-user-reports/
+      event_id: this.eventId,
+    }
   }
 }
 
