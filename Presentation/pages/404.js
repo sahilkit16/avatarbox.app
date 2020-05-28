c3825db37482bbf8f7b759b8d6661f8f433105f7
Stephen Tillman <askmrtillman@gmail.com>
2020-05-28 09:35:10 -0400

    link error stylesheet

--- Presentation/pages/404.js
+++ Presentation/pages/404.js
@@ -2,6 +2,7 @@ import HeroHead from "../components/hero-head";
 import HeroSection from "../components/hero-section";
 import ErrorBody from "../components/error-body";
 import ErrorVM from "../view-models/error.vm";
+import Head from "next/head";
 
 export default function Custom404() {
   const model = new ErrorVM();
@@ -10,6 +11,9 @@ export default function Custom404() {
   model.message = "The page you are looking for could not be found.";
   return (
     <HeroSection hideCoverImage="true">
+      <Head>
+        <link rel="stylesheet" type="text/css" href="../css/error.css" />
+      </Head>
       <HeroHead title={model.title} navbar={model.navbar} />
       <ErrorBody title={model.subtitle} message={model.message} />
     </HeroSection>
