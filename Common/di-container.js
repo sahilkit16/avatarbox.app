0d6e3f797663d81fe511e84509a129b36db9ec68
Stephen Tillman <askmrtillman@gmail.com>
2020-05-29 01:30:14 -0400

    add pusher client

--- Common/di-container.js
+++ Common/di-container.js
@@ -9,6 +9,7 @@ const CacheService = require("../Services/cache.service");
 const BuildCalendarUseCase = require("../Application/build-calendar.use-case");
 const RsaService = require("../Services/rsa.service");
 const UserService = require("../Services/user.service");
+const PusherClient = require("../Infrastructure/pusher.client");
 
 container.register({
   dataStore: awilix.asClass(DataStore),
@@ -21,6 +22,7 @@ container.register({
   buildCalendar: awilix.asClass(BuildCalendarUseCase),
   rsaService: awilix.asClass(RsaService),
   userService: awilix.asClass(UserService),
+  pusherClient: awilix.asClass(PusherClient),
 });
 
 module.exports = container;
---
+++ Infrastructure/pusher.client.js
@@ -0,0 +1,17 @@
+const Pusher = require("pusher-js");
+
+class PusherClient {
+  constructor(){
+    this.channel = null;
+    this.self = new Pusher(process.env.PUSHER_KEY, {
+      cluster: process.env.PUSHER_CLUSTER,
+    })
+  }
+  subscribe(emailHash, handler){
+    this.channel = this.self.subscribe(emailHash);
+    this.channel.bind("update-event", handler);
+  }
+}
+
+module.exports = PusherClient;
+
