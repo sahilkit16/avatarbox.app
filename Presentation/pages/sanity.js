8ad05bc54686487b260d4e8914d5705ef1f4c9bf
Stephen Tillman <askmrtillman@gmail.com>
2020-05-29 09:48:18 -0400

    use view model, replace get-initial-props with get-server-side-props

--- Presentation/pages/sanity.js
+++ Presentation/pages/sanity.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Error from "./_error";
 import CrashReporter from "../../Common/crash-reporter.client";
+import ThanksVM from "../view-models/thanks.vm";
+
+class SanityVM extends ThanksVM {
+  constructor(){
+    super();
+    this.appziScript = null;
+  }
+}
 
 function BrokenComponent({ breakit }) {
   if (breakit) {
     throw new Error("broken child component");
   }
   return <div />;
+}
+
+export async function getServerSideProps(context) {
+  const model = new SanityVM();
+  if (context.req.query["server-error"]) {
+    throw new Error("this is a test");
+  }
+  return {
+    props: model.toObject()
+  };
 }
 
 class SanityPage extends React.Component {
@@ -44,30 +62,25 @@ class SanityPage extends React.Component {
       return <Error eventId={this.state.eventId} />;
     }
     return (
-      <div>
-        <p>
-          <a href="/sanity/server-error">Trigger ExpressJS Server Error</a>
-        </p>
-        <p>
-          <a href="/sanity?server-error=1">Trigger NextJS Server Error</a>
-        </p>
-        <p>
-          <button onClick={this.jsError}>Trigger Javascript Error</button>
-        </p>
-        <p>
-          <button onClick={this.reactError}>Trigger React Error</button>
-        </p>
-        <BrokenComponent breakit={this.state.breakit} />
+      <div className="hero-body">
+        <div className="container">
+          <p>
+            <a href="/sanity/server-error">Trigger ExpressJS Server Error</a>
+          </p>
+          <p>
+            <a href="/sanity?server-error=1">Trigger NextJS Server Error</a>
+          </p>
+          <p>
+            <button onClick={this.jsError}>Trigger Javascript Error</button>
+          </p>
+          <p>
+            <button onClick={this.reactError}>Trigger React Error</button>
+          </p>
+          <BrokenComponent breakit={this.state.breakit} />
+        </div>
       </div>
     );
   }
 }
-
-SanityPage.getInitialProps = async ({ req }) => {
-  if (req.query["server-error"]) {
-    throw new Error("this is a test");
-  }
-  return {};
-};
 
 export default SanityPage;
