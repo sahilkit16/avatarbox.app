8913e1ecc642af0ae2f214f64d6320aa66f515f1
Stephen Tillman <askmrtillman@gmail.com>
2020-05-30 14:40:28 -0400

    install react-ga; install tracking code

--- Presentation/components/hero-head.js
+++ Presentation/components/hero-head.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Head from "next/head";
 import NavBar from "./navbar";
+import ReactGA from "react-ga";
 
 function HeroHead({ title, navbar }) {
+  ReactGA.initialize(process.env.GA_TRACKING_CODE, { 
+    debug: !!process.env.DEV_ENV 
+  })
   return (
     <div className="hero-head">
       <Head>
