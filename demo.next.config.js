5915ad409ad225ed1f9582b79122e43cb7f5faca
Stephen Tillman <askmrtillman@gmail.com>
2020-05-31 01:44:26 -0400

    tweak demo configs

--- demo.next.config.js
+++ demo.next.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   env: {
+    'AMQP_URI': 'amqp://localhost:5672',
     'DEV_ENV': true,
-    'SENTRY_DSN': 'https://000@111.ingest.sentry.io/222'
+    'GA_TRACKING_CODE': 'XX-000000000-0',
+    'MONGO_URI': 'mongodb://localhost:27017',
+    'PRIVATE_KEY_PATH': '/path/to/private/key',
+    'PUBLIC_KEY_PATH': '/path/to/public/key',
+    'PUSHER_KEY': '11111112222222333333',
+    'PUSHER_CLUSTER': 'xx1',
+    'REDIS_URL': 'redis://localhost:6379',
+    'SANITY': true,
+    'SENTRY_DSN': 'https://000@111.ingest.sentry.io/222',
+    'SESSION_KEY': 'all that glitters isn\'t gold',
   }
 }
