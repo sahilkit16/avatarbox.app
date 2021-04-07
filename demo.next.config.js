87f970bbb66ba3aaa6bc57e4ed131cf3dc6f79c9
Stephen Tillman <askmrtillman@gmail.com>
2021-04-07 18:27:36 -0400

    remove sanity environment variable

--- demo.next.config.js
+++ demo.next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     'PUSHER_KEY': '11111112222222333333',
     'PUSHER_CLUSTER': 'xx1',
     'REDIS_URI': 'redis://localhost:6379',
-    'SANITY': true,
     'SENTRY_DSN': 'https://000@111.ingest.sentry.io/222',
     'SESSION_KEY': 'a rolling stone gathers no moss',
   }
