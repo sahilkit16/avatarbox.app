c37264df707931a08daae81e93d0b3e061e2a97f
Stephen Tillman <askmrtillman@gmail.com>
2021-04-08 16:29:55 -0400

    include content-type header in ajax check

--- Presentation/middleware/is-ajax.js
+++ Presentation/middleware/is-ajax.js
@@ -1,4 +1,4 @@
 export function isAjax(req, res, next) {
-  req.isAjax = req.xhr || /json/i.test(req.headers.accept);
+  req.isAjax = req.xhr || /json/i.test(req.headers.accept) || /json/i.test(req.headers["content-type"])
   next();
 }
