1a00d59a0403722270109d6c43d3497db487c55a
Stephen Tillman <askmrtillman@gmail.com>
2021-04-10 20:00:41 -0400

    convert values to data types understood by the redis client

--- Services/cache.service.js
+++ Services/cache.service.js
@@ -1,24 +1,39 @@
 import { RedisClient } from "../Infrastructure/redis.client";
+import { isJson } from "../Common/helpers";
 
 export class CacheService {
   constructor() {
     this._cache = new RedisClient();
   }
-  async get(value) {
-    const _value = await this._cache.get(value);
-    return JSON.parse(_value);
+  unbox(value) {
+    const jsonValue = isJson(value);
+    if (jsonValue) return jsonValue;
+    if (value instanceof Date) return value;
+    if (!isNaN(value)) return Number(value);
+    return value;
+  }
+  box(value) {
+    if (value instanceof Date) return value;
+    if (value instanceof Object) return JSON.stringify(value);
+    if (!isNaN(value)) return value.toString();
+    return value;
+  }
+  async get(key) {
+    const value = await this._cache.get(key);
+    return this.unbox(value);
   }
   async set(key, value) {
-    return await this._cache.set(key, value);
+    return await this._cache.set(key, this.box(value));
   }
   async delete(key) {
     return await this._cache.delete(key);
   }
   async hset(key, field, value) {
-    return await this._cache.hset(key, field, value);
+    return await this._cache.hset(key, field, this.box(value));
   }
   async hget(key, field) {
-    return await this._cache.hget(key, field);
+    const value = await this._cache.hget(key, field);
+    return this.unbox(value);
   }
   async hdel(key, field) {
     return await this._cache.hdel(key, field);
