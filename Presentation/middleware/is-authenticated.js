875ed8cd2878202926351a89a136ae32296ce64a
Stephen Tillman <askmrtillman@gmail.com>
2021-05-09 00:44:39 -0400

    check for passport user

--- Presentation/middleware/is-authenticated.js
+++ Presentation/middleware/is-authenticated.js
@@ -1,7 +1,7 @@
 import { redirect } from "next/dist/next-server/server/api-utils";
 
 export function isAuthenticated(req, res, next) {
-  if (!req.session.user) {
+  if (!req.session.user && !req.session.passport.user) {
     return redirect(res, "/");
   }
   next();
