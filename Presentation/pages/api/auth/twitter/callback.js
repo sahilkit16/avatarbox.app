601a05a7c37abf09903474accec2e4db9c1759c0
Stephen Tillman <askmrtillman@gmail.com>
2021-05-09 00:44:18 -0400

    save twitter user in session; redirect to calendar page; handle any callback errors

--- Presentation/pages/api/auth/twitter/callback.js
+++ Presentation/pages/api/auth/twitter/callback.js
@@ -2,17 +2,23 @@ import { use, passportMiddleware } from "../../../../middleware";
 import { container } from "../../../../../Common/di-container";
 import { withSession } from "next-session";
 
-const handler = async (req, res, next) => {
-  passportMiddleware.authenticate('twitter', function (err, user, info) {
-    if (err) { return next(err); }
-    console.log(user);
-    // if (!user) { return res.redirect('/'); }
-    // req.logIn(user, function (err) {
-    //   if (err) { return next(err); }
-    //   res.redirect('/users');
-    // });
-  })(req, res, next);
-}
+const handler = async (req, res) => {
+  await use(req, res, [passportMiddleware.initialize()]);
+  passportMiddleware.authenticate("twitter", function (err, user, info) {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      return res.redirect("/");
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        throw err;
+      }
+      res.redirect("/calendar");
+    });
+  })(req, res);
+};
 
 const cache = container.resolve("cacheService");
 
