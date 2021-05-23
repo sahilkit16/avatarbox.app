0d5712c9bf82b6780ec0bbc73dc1e2876d025fb4
Stephen Tillman <askmrtillman@gmail.com>
2021-05-23 18:57:14 -0400

    implement local passport module for gravatar

--- Presentation/middleware/passport.gravatar.js
+++ Presentation/middleware/passport.gravatar.js
@@ -1,16 +1,38 @@
 import * as passport from "passport";
 import * as LocalStrategy from "passport-local";
-import { container } from "../../Common/di-container";
+import { LoginVM } from "../view-models/login.vm";
+import ShortId from "shortid";
+import { AvbxGravatarClient } from "avatarbox.sdk";
 
-var strategy = new LocalStrategy(async function (username, password, done) {
-  const avbx = container.resolve("avbx");
-  const client = await avbx.login(username, password);
-  if (!client) {
-    return done(null, false, { message: "Invalid username or password." });
-  }
-  return done(null, client);
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async function (username, password, done) {
+      const loginVm = new LoginVM();
+      loginVm.email = username;
+      loginVm.password = password;
+      const message = loginVm.errors.email || loginVm.errors.password;
+      if (message) return done(null, false, { message });
+      const avbx = new AvbxGravatarClient();
+      const client = await avbx.login(username, password);
+      if (!client)
+        return done(null, false, { message: "Invalid username or password." });
+      const user = { email: loginVm.email };
+      user.hash = client.emailHash;
+      user.cacheBuster = ShortId();
+      return done(null, user);
+    }
+  )
+);
+
+passport.serializeUser(async function (user, done) {
+  const avbx = new AvbxGravatarClient();
+  const { id, lastUpdated } = await avbx.user.find(user.email);
+  user.id = id;
+  user.lastUpdated = lastUpdated;
+  done(null, user);
 });
-
-passport.use(strategy);
 
 export const passportGravatar = passport;
