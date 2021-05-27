19cb890e7a077c9ad7c9a90c9244bb8cda556d9b
Stephen Tillman <askmrtillman@gmail.com>
2021-05-26 20:28:08 -0400

    add rotateLeft method from build calendar use case

--- Common/helpers.js
+++ Common/helpers.js
@@ -5,3 +5,20 @@ export const isJson = (stringValue) => {
     return false;
   }
 };
+
+export function rotateLeft(collection, _targetIndex) {
+  const targetIndex = _targetIndex % collection.length;
+  if (targetIndex == 0) {
+    return collection;
+  }
+  const begin = [];
+  const end = [];
+  collection.map((item, index) => {
+    if (index >= targetIndex) {
+      begin.push(item);
+    } else {
+      end.push(item);
+    }
+  });
+  return [...begin, ...end];
+}
