8a483835551c47fdb7d472c65a061e8b5db5e505
Stephen Tillman <askmrtillman@gmail.com>
2021-06-02 19:10:42 -0400

    scaffold desktop sidebar

---
+++ Presentation/components/desktop-sidebar.js
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from "react-redux";
+import actionTypes from "../actions/action-types";
+
+function MobileSidebar() {
+  const { menu, selectedIcon } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const closeMenu = () => {
+    dispatch({
+      type: actionTypes.CLOSE_MENU,
+    });
+  };
+  const deleteIcon = () => {
+    if (confirm("Are you sure?")) {
+      document.getElementById("menu-form").submit();
+    }
+  };
+  return (
+    <div className={menu && menu.visible ? "desktop-sidebar" : "is-hidden"}>
+      <input
+        type="hidden"
+        name="selectedIcon"
+        value={selectedIcon && selectedIcon.url}
+      />
+      <article className="panel">
+        <div className="panel-block">
+          <figure className="image is-64x64">
+            <img className="is-square" src={selectedIcon && selectedIcon.url} />
+          </figure>
+        </div>
+        <a className="panel-block" onClick={deleteIcon}>
+          DELETE
+        </a>
+        <a className="panel-block" href="#">
+          UPLOAD
+        </a>
+        <a className="panel-block" onClick={closeMenu}>
+          CLOSE
+        </a>
+      </article>
+    </div>
+  );
+}
+
+export default MobileSidebar;
