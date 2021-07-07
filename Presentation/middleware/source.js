d67e3eaa70bbb8c72dac919b62d4f813d4ffc7c4
Stephen Tillman <askmrtillman@gmail.com>
2021-07-07 19:10:47 -0400

    simplify source middleware

--- Presentation/middleware/source.js
+++ Presentation/middleware/source.js
@@ -1,14 +1,8 @@
-import { use } from ".";
-import { isAuthenticated } from "./is-authenticated";
-
 export async function source(req, res, next) {
-  req.source = req.session.passport.user.source;
-  await use(req, res, [isAuthenticated]);
-  if (req.source) next();
+  if(!req.session || !req.session.passport) return next();
+  req.source = "gravatar";
   if (req.session.passport.user.token) {
     req.source = "twitter";
-  } else {
-    req.source = "gravatar";
   }
   next();
 }
